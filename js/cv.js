6fc4c4741e2b1d26a0b242cc2dd0d31b717d41eb
Koijari <148049848+Koijari@users.noreply.github.com>
2024-04-03 14:15:38 +0300

    Add files via upload

---
+++ js/cv.js
@@ -0,0 +1,16 @@
+var kuvat = [
+    "/kuvat/Solo_Tech.png",
+    "/kuvat/Solo_HTML.png",
+    "/kuvat/Solo_CSS.png",
+    "/kuvat/Solo_JavaScript.png",
+    "/kuvat/Solo_Python.png"
+];
+let nro =0;
+function seuraava() {
+    var esitys = document.getElementById("esitys");
+    nro++;
+    if (nro >= kuvat.length) {
+        nro = 0;
+    };
+    esitys.src = kuvat[nro];
+};
