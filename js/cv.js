77964031af169660aaf5e6923179f009fb5f023e
Koijari <148049848+Koijari@users.noreply.github.com>
2024-04-03 14:33:34 +0300

    Update cv.js

--- js/cv.js
+++ js/cv.js
@@ -1,9 +1,9 @@
 var kuvat = [
-    "/kuvat/Solo_Tech.png",
-    "/kuvat/Solo_HTML.png",
-    "/kuvat/Solo_CSS.png",
-    "/kuvat/Solo_JavaScript.png",
-    "/kuvat/Solo_Python.png"
+    "../kuvat/Solo_Tech.png",
+    "../kuvat/Solo_HTML.png",
+    "../kuvat/Solo_CSS.png",
+    "../kuvat/Solo_JavaScript.png",
+    "../kuvat/Solo_Python.png"
 ];
 let nro =0;
 function seuraava() {
